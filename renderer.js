446902c72fc4504c7ee0dc586cca2cb75dffff2e
cykim8811 <cykim8811@users.noreply.github.com>
2020-07-29 06:52:15 +0900

    Renderer Class Constructor

--- renderer.js
+++ renderer.js
@@ -1,8 +1,13 @@
-class Renderer{
-    constructor(
-        
-    )
-    {
 
+class Renderer{
+    constructor(_window, _document){
+        this.scene = new THREE.Scene();
+        this.renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        this.renderer.setSize(_window.innerWidth, _window.innerHeight);
+        _document.body.appendChild(this.renderer.domElement);
+        //_document.addEventListener('keydown', ~~);
+        this.renderer.setClearColor(0x000000, 1);
     }
 }
